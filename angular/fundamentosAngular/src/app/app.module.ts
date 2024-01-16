8d40d5dbf07410d8a2c09d6c3d251e487389b306
TiagoFacisa <tiago.maior@maisunifacisa.com.br>
2024-01-16 19:05:46 -0300

    Create: creating firt components

--- angular/fundamentosAngular/src/app/app.module.ts
+++ angular/fundamentosAngular/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponent } from './firstComponent/firstComponent.Component';
+import { SecondComponentComponent } from './second-component/second-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+    SecondComponentComponent
   ],
   imports: [
     BrowserModule,
---
+++ angular/fundamentosAngular/src/app/firstComponent/firstComponent.Component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-firstComponent",
+    template: "<h2>First Component</h2>", 
+    styles: ["h2 { color: red; }"]
+})
+
+export class FirstComponent {}
